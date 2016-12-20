022356bfaa894238877651050f8a8596b00050b6
cpaisan <cppaisan@gmail.com>
2016-12-19 17:59:27 -0800

    first draft templates completed

--- apps/ecommerce/views.py
+++ apps/ecommerce/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'ecommerce/index.html')
+
+def product(request):
+    return render(request, 'ecommerce/product.html')
+
+def admin(request):
+    return render(request, 'ecommerce/admin.html')
+
+def orders(request):
+    return render(request, 'ecommerce/orders.html')
+
+def products(request):
+    return render(request, 'ecommerce/products.html')
+
+def show(request):
+    return render(request, 'ecommerce/show.html')
